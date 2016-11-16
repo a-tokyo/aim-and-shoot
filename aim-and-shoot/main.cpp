396deb0982e936bbbbf0d26f81a568335053c2f7
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-16 18:55:45 +0200

    Initial Commit

---
+++ aim-and-shoot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aim-and-shoot
+//
+//  Created by Ahmed Mohamed on 11/16/16.
+//  Copyright © 2016 Ahmed Mohamed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
